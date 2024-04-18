a94b553d7c60ef923cf7f01670c6698adf70659d
Lalasa1234 <166985767+Lalasa1234@users.noreply.github.com>
2024-04-18 20:22:39 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import numpy as np
 from catboost import CatBoostClassifier
 import sklearn
 
-model_path = "Model/catboost.joblib"
+model_path = "Model\catboost.joblib"
 model = joblib.load(model_path)
 
 def predict(data):
